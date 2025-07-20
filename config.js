471e2a6c0fc1935162e0a33ea41f2fe36120c5ba
Vicky816-tech <adeoyedavid.134@gmail.com>
2025-07-20 11:24:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
     CHATBOT: getConfig("CHATBOT") || "on",
     //chatbot on/off
   
-    SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~9VRGWSib#i79cNfsGhEwC1leHBMvxf7Stq9uHkhDIUuzWy9EVcy0",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~3V9zSCQb#Jj3_2p_riO-KU8uFvK2p0uLeT4qZSikJIlHHdGEGKLU",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "MEGALODON-MD",  // Bot's display name
     MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
